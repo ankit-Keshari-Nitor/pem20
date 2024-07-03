1299314847704c0a38ca0deb134c66b21de8f8d0
Gopal Gupta <gopal.gupta@nitorinfotech.net>
2024-07-03 10:41:35 +0530

    table row feature added

--- pem-ui/packages/carbon-mappers/src/constant/field-property-props.js
+++ pem-ui/packages/carbon-mappers/src/constant/field-property-props.js
@@ -207,7 +207,9 @@ export const selectRow = {
   propsName: 'selectablerows',
   label: 'Selectable Rows',
   value: false,
-  type: 'Toggle'
+  type: 'Toggle',
+  labelA: 'Yes',
+  labelB: 'No'
 };
 
 export const pageSize = {
